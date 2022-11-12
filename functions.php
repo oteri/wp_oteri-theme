9602503e421fa455463d54ce0d475c2bef757f68
Francesco  OTERI <f.oteri@instadeep.com>
2022-11-12 15:55:53 +0100

    refactoring

--- functions.php
+++ functions.php
@@ -1,42 +1,54 @@
 <?php 
- function oteri_banner_option($wp_customize){
-//=================================================================
-// Add Banner Uploader 
-//=================================================================
-    $wp_customize->add_setting( 'oteri_banner', array(
-        'default' => get_theme_file_uri('assets/image/banner.jpg'), // Add Default Image URL 
-        'sanitize_callback' => 'esc_url_raw'
-    ));
- 
-    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'oteri_banner_control', array(
-        'label' => 'Upload Banner',
-        'priority' => 20,
-        'section' => 'title_tagline',
-        'settings' => 'oteri_banner',
-        'button_labels' => array(// All These labels are optional
-                    'select' => 'Select Banner',
-                    'remove' => 'Remove Banner',
-                    'change' => 'Change Banner',
-                    )
-    )));
- }
-
- function oteri_social_network_options($wp_customize){
-    $supported_social_networks = array("Github", "Linkedin", "Twitter", "Medium");
-
-    //=================================================================
-    // Add Social network profiles
-    //=================================================================
+function add_customization_panel($wp_customize){
     $wp_customize->add_panel( 'oteri_option_panel', array(
-        'title' => 'Social Networks',
-        'description' => 'oteri Options help you to customize your site.',
+        'title' => 'Customize oteri theme',
+        'description' => 'Customize your site.',
         'capability' => 'edit_theme_options', 
         'theme_supports' => '',
         'active_callback' => ''
     ));
 
-    $wp_customize->add_section( 'oteri_social_network_section', array(
-        'title' => 'Profiles',
+}
+//=================================================================
+// Add Banner and Profile picture Uploader 
+//=================================================================
+ function oteri_banner_option($wp_customize){
+    $wp_customize->add_section( 'oteri_front_page_assets', array(
+        'title' => 'Home Page',
+        'description' => 'Customize the front page',
+        'panel' => 'oteri_option_panel',
+        'capability' => 'edit_theme_options',
+        'theme_supports' => '',
+        'active_callback' => '',
+        'description_hidden' => false
+    ));
+
+//Banner
+    $wp_customize->add_setting( 'oteri_banner', array(
+        'default' => "", // Add Default Image URL 
+        'sanitize_callback' => 'esc_url_raw'
+    ));
+ 
+    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'oteri_banner_control', array(
+        'label' => 'Upload Banner',
+        'section' => 'oteri_front_page_assets',
+        'settings' => 'oteri_banner',
+        'button_labels' => array(// All These labels are optional
+                    'select' => 'Select Image',
+                    'remove' => 'Remove Image',
+                    'change' => 'Change Image',
+                    )
+    )));
+
+
+ }
+
+    //=================================================================
+    // Add Social network profiles
+    //=================================================================
+ function oteri_social_network_options($wp_customize){
+    $wp_customize->add_section( 'oteri_customization', array(
+        'title' => 'Social network profiles',
         'description' => 'Add the Social Network Profiles to show on the Home Page',
         'panel' => 'oteri_option_panel',
         'capability' => 'edit_theme_options',
@@ -45,6 +57,8 @@
         'description_hidden' => false
     ));
 
+
+    $supported_social_networks = array("Github", "Linkedin", "Twitter", "Medium"); 
     foreach ($supported_social_networks as $network) {          
             $name = 'oteri_'. strtolower($network);       
             $wp_customize->add_setting( $name, array(
@@ -53,13 +67,13 @@
             ));
             $wp_customize->add_control( $name, array(
                 'label' => $network,
-                'section' => 'oteri_social_network_section',
+                'section' => 'oteri_customization',
                 'settings' => $name,
                 'type' => 'text',
                 'input_attrs' => array( 'placeholder' => 'Account URL' )
             ));
     }
 }
-
+add_action( 'customize_register', 'add_customization_panel' );
 add_action( 'customize_register', 'oteri_banner_option' );
 add_action( 'customize_register', 'oteri_social_network_options' );
