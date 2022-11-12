baa183706df5d6dd933cb5a246dddcb6dbe8aa14
Francesco  OTERI <f.oteri@instadeep.com>
2022-11-12 11:57:23 +0100

    Adding a banner to the theme

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+ 
+ //=================================================================
+ // Add Banner Uploader 
+ //=================================================================
+function oteri_theme_customizer_options($wp_customize){
+ 
+    $wp_customize->add_setting( 'oteri_banner', array(
+        'default' => get_theme_file_uri('assets/image/banner.jpg'), // Add Default Image URL 
+        'sanitize_callback' => 'esc_url_raw'
+    ));
+ 
+    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'oteri_banner_control', array(
+        'label' => 'Upload Banner',
+        'priority' => 20,
+        'section' => 'title_tagline',
+        'settings' => 'oteri_banner',
+        'button_labels' => array(// All These labels are optional
+                    'select' => 'Select Banner',
+                    'remove' => 'Remove Banner',
+                    'change' => 'Change Banner',
+                    )
+    )));
+ 
+}
+ 
+add_action( 'customize_register', 'oteri_theme_customizer_options' );
