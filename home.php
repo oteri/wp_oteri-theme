95a5a8b1c7027e70834f2449b4ca51752602776d
Francesco  OTERI <f.oteri@instadeep.com>
2022-11-15 02:20:52 +0100

    Added menu on home page

--- home.php
+++ home.php
@@ -10,7 +10,45 @@
     </head>
 
     
-<div class=header_container><img alt="" src="<?php header_image(); ?>" ></div>
+<div class=header_container>
+  <img alt="" src="<?php header_image(); ?>" >
+
+
+
+<?php
+$menu=array('About');
+$args = array(
+   'public'   => true,
+   '_builtin' => false
+);
+ 
+$output = 'names'; // 'names' or 'objects' (default: 'names')
+$operator = 'and'; // 'and' or 'or' (default: 'and')
+ 
+$post_types = get_post_types( $args, $output, $operator );
+if ( $post_types ) { // If there are any custom public post types.
+     foreach ( $post_types  as $post_type ) {
+        array_push( $menu, ucfirst($post_type));
+    }
+  }
+
+ $menu = array_merge($menu,array('Blog', 'Contact'));
+ echo "<div class=menu>";
+ foreach ($menu as $item) { 
+  echo '<div class=item><a target=_blank href=/'.$item.'>'.$item.'</a></div>';
+ }
+ echo "</div>";
+?>
+
+
+
+
+
+
+
+
+
+</div>
 <img class="profile_photo" alt="" src="<?php echo get_theme_mod("oteri_profile_photo"); ?>">
 <div class="description"><?php echo get_theme_mod("oteri_description") ?></div>
 <div class=social_network_profiles_menu>
