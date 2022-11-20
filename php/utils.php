3a75c8b975a9fb920aa1f18855d3bd4fc1b043aa
Francesco  OTERI <f.oteri@instadeep.com>
2022-11-20 22:26:43 +0100

    menu generation made easier

--- php/utils.php
+++ php/utils.php
@@ -1,26 +1,9 @@
 <?php
 function get_navigation_menu(){
-        $menu=array();
-        $args = array(
-        'public'   => true,
-        '_builtin' => false
-        );
-        
-        $output = 'names'; // 'names' or 'objects' (default: 'names')
-        $operator = 'and'; // 'and' or 'or' (default: 'and')
-        
-        $post_types = get_post_types( $args, $output, $operator );
-        if ( $post_types ) { // If there are any custom public post types.
-            foreach ( $post_types  as $post_type ) {
-                array_push( $menu, ucfirst($post_type));
-            }
-        }
-
-        $menu = array_merge($menu,array('Blog'));
+        $menu = array("Home"=>"", "Papers"=>"Papers", "Softwares"=>"Softwares", "Blog"=>"Blog");        
         echo "<div class=menu>";
-        echo '<div class=item><a target=_blank href=/>Home</a></div>';
-        foreach ($menu as $item) { 
-            echo '<div class=item><a target=_blank href=/'.$item.'>'.$item.'</a></div>';
+        foreach ($menu as $name => $url) { 
+            echo '<div class=item><a target=_blank href=/'.$url.'>'.$name.'</a></div>';
         }
         echo "</div>";
 }
