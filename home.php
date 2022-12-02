1a4040de40244afbd88b1aea0f8b55a7519c3925
Francesco  OTERI <f.oteri@instadeep.com>
2022-12-02 16:17:40 +0100

    custom css after predefined ones

--- home.php
+++ home.php
@@ -4,9 +4,9 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 <title><?php wp_title(''); ?></title>
+<?php wp_head(); ?>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri().'/css/style.css' ?>" type="text/css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri().'/css/home.css' ?>" type="text/css">
-<?php wp_head(); ?>
 </head>
 <body>
 
