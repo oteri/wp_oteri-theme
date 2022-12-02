2597aceef45223d56945df344c53429a9f7fe02b
Francesco  OTERI <f.oteri@instadeep.com>
2022-12-02 17:15:55 +0100

    navigation bar reactive

--- php/utils.php
+++ php/utils.php
@@ -1,59 +1,25 @@
 <?php
-// TO DO: Moving menu generation to the admin interface rather then hard coding it
 function get_navigation_menu(){
-    $items = array("Home"=>"", "Papers"=>"Papers", "Software"=>"Software", "Blog"=>"Blog");        
-    get_menu($items);
-    get_navbar($items);
-}
-//Print the menu which is activated on PC
-function get_menu($menu){
-  echo '<ul class="navbar nav bg-light justify-content-center">';
-    foreach ($menu as $name => $url) { 
-        echo '<li class="nav-item"><a class="nav-link" aria-current="page" href=/'.$url.'>'.$name.'</a></li>';
-    }
-    echo '</ul>';
-}
+    // TO DO: Moving menu generation to the admin interface rather then hard coding it
+    $menu = array("Home"=>"", "Papers"=>"Papers", "Software"=>"Software", "Blog"=>"Blog");        
+    $brand_name = 'Francesco Oteri';
 
-//Print the navbar which is activated on mobile
-function get_navbar($menu){
-echo '<nav class="navbar bg-light fixed-top">
-<div class="container-fluid justify-content-end">
-  <button
-    class="navbar-toggler"
-    type="button"
-    data-bs-toggle="offcanvas"
-    data-bs-target="#offcanvasNavbar"
-    aria-controls="offcanvasNavbar"
-  >
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div
-    class="offcanvas offcanvas-end"
-    tabindex="-1"
-    id="offcanvasNavbar"
-    aria-labelledby="offcanvasNavbarLabel"
-  >
-    <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-      <button
-        type="button"
-        class="btn-close"
-        data-bs-dismiss="offcanvas"
-        aria-label="Close"
-      ></button>
-    </div>
-    <div class="offcanvas-body">
-      <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">';       
-        foreach ($menu as $name => $url) { 
-            echo '<li class="nav-item"><a class="nav-link" aria-current="page" href=/'.$url.'>'.$name.'</a></li>';
-        }
+    echo '<nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">'.$brand_name.'</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul class="navbar-nav">';
 
-echo '
-      </ul>
-    </div>
-  </div>
-</div>
-</nav>
-';
+            foreach ($menu as $name => $url) { 
+                echo '<li class="nav-item ms-auto"><a class="nav-link" aria-current="page" href=/'.$url.'>'.$name.'</a></li>';
+                } 
+        echo '
+          </ul>
+        </div>
+      </div>
+    </nav>';
 }
 ?>
