d63876fe706058202931248245d8db6a74360f54
Francesco  OTERI <f.oteri@instadeep.com>
2022-12-02 16:19:02 +0100

    Adding toggling menu for mobile

--- php/utils.php
+++ php/utils.php
@@ -1,11 +1,49 @@
 <?php
 // TO DO: Moving menu generation to the admin interface rather then hard coding it
 function get_navigation_menu(){
-        $menu = array("Home"=>"", "Papers"=>"Papers", "Software"=>"Software", "Blog"=>"Blog");        
-        echo "<div class=menu>";
+    $items = array("Home"=>"", "Papers"=>"Papers", "Software"=>"Software", "Blog"=>"Blog");        
+    get_navbar($items);
+}
+//Print the navbar which is activated on mobile
+function get_navbar($menu){
+echo '<nav class="navbar bg-light fixed-top">
+<div class="container-fluid justify-content-end">
+  <button
+    class="navbar-toggler"
+    type="button"
+    data-bs-toggle="offcanvas"
+    data-bs-target="#offcanvasNavbar"
+    aria-controls="offcanvasNavbar"
+  >
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div
+    class="offcanvas offcanvas-end"
+    tabindex="-1"
+    id="offcanvasNavbar"
+    aria-labelledby="offcanvasNavbarLabel"
+  >
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+      <button
+        type="button"
+        class="btn-close"
+        data-bs-dismiss="offcanvas"
+        aria-label="Close"
+      ></button>
+    </div>
+    <div class="offcanvas-body">
+      <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">';       
         foreach ($menu as $name => $url) { 
-            echo '<div class=item><a target=_blank href=/'.$url.'>'.$name.'</a></div>';
+            echo '<li class="nav-item"><a class="nav-link" aria-current="page" href=/'.$url.'>'.$name.'</a></li>';
         }
-        echo "</div>";
+
+echo '
+      </ul>
+    </div>
+  </div>
+</div>
+</nav>
+';
 }
 ?>
