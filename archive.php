b6a6021ebed22b5615c70dda167139aa5e78c560
Francesco  OTERI <f.oteri@instadeep.com>
2022-12-03 17:13:05 +0100

    responsiveness almost ready

--- archive.php
+++ archive.php
@@ -24,31 +24,25 @@
                 $name = $wp_query->query['post_type'];
               echo "<h2 class=message> List of the ".$wp_query->post_count." ".$name." I have authored or co-authored so far</h2>" 
             ?>
-            <div id="loop">
-               <?php while(have_posts()) : the_post(); ?>
+            <div class ="container"id="loop">
+               <?php while(have_posts()) : the_post(); ?>              
+               <div class="row">
+                      <div class="col-sm-12 col-md-12 col-lg-4 text-center">
 
+                              <?php 
+                                  if ( has_post_thumbnail() ) {
+                                        the_post_thumbnail($size = 'medium');
+                                      }   
+                              ?>
 
-               <div class="card mb-3">
-                  <div class="row g-0">
-                    <div class="col-sm-4 col-md-4 mb-auto mt-auto">
-                        <div class="text-center p-4" > 
-                          <?php 
-                              if ( has_post_thumbnail() ) {
-                                    the_post_thumbnail($size = 'medium');
-                                  }   
-                          ?>
-                        </div>
-                    </div>
-                    <div class="col-sm-8 col-md-8">
-                      <div class="card-body">
-                        <h5 class="card-title"><?php the_title(); ?></h5>
-                        <p class="card-text"><?php the_content(); ?></p>
                       </div>
-                   </div>
-                  </div>
+                      <div class="col-sm-12 col-md-12 col-lg-8">
+                        <h5 ><?php the_title(); ?></h5>
+                        <p ><?php the_content(); ?></p>
+                      </div>
               </div>
-                <?php endwhile; ?>
-            </div>
+            <?php endwhile; ?>
+          </div>
         <?php else : ?>
             <h2> No content available</h2>
         <?php endif; ?>
